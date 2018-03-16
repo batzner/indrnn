b55f6fa123dc0aebdfdf23c857e1d65050638712
batzner <git@kilians.net>
2018-03-16 17:52:32 +0100

    Add test without recurrent weights exceeding the bounds

--- ind_rnn_cell_test.py
+++ ind_rnn_cell_test.py
@@ -13,6 +13,24 @@ from ind_rnn_cell import IndRNNCell
 
 class IndRNNCellTest(test.TestCase):
   def testIndRNNCell(self):
+    """Tests basic cell functionality"""
+
+    with self.test_session() as sess:
+      with variable_scope.variable_scope(
+          "root", initializer=init_ops.constant_initializer(1.)):
+        x = array_ops.zeros([1, 4])
+        m = array_ops.zeros([1, 4])
+        recurrent_init = init_ops.constant_initializer([-3., -2., 1., 3.])
+        cell = IndRNNCell(4, recurrent_initializer=recurrent_init,
+                          activation=array_ops.identity)
+        output, _ = cell(x, m)
+        sess.run([variables.global_variables_initializer()])
+        res = sess.run([output], {x.name: np.array([[1., 0., 0., 0.]]),
+          m.name: np.array([[2., 2., 2., 2.]])})
+        # (Pre)activations (1*1 + 2*rec_weight) should be -2, 0, 6, 10
+        self.assertAllEqual(res[0], [[-5., -3., 3., 7.]])
+
+  def testIndRNNCellBounds(self):
     """Tests cell with recurrent weights exceeding the bounds."""
 
     with self.test_session() as sess:
@@ -22,12 +40,12 @@ class IndRNNCellTest(test.TestCase):
         m = array_ops.zeros([1, 4])
         recurrent_init = init_ops.constant_initializer([-5., -2., 0.1, 5.])
         cell = IndRNNCell(4, recurrent_min_abs=1., recurrent_max_abs=3.,
-                          recurrent_initializer=recurrent_init)
+                          recurrent_initializer=recurrent_init,
+                          activation=array_ops.identity)
         output, _ = cell(x, m)
         sess.run([variables.global_variables_initializer()])
-        res = sess.run([output], {x.name: np.array([[1., 1., 1., 1.]]),
+        res = sess.run([output], {x.name: np.array([[1., 0., 0., 0.]]),
           m.name: np.array([[2., 2., 2., 2.]])})
         # Recurrent Weights should be -3, -2, 1, 3
-        # (Pre)activations (4 + 2*rec_weight) should be -2, 0, 6, 10
-        # Thus, ReLU outputs should be 0, 0, 6, 10
-        self.assertAllEqual(res[0], [[0., 0., 6., 10.]])
+        # (Pre)activations (1*1 + 2*rec_weight) should be -2, 0, 6, 10
+        self.assertAllEqual(res[0], [[-5., -3., 3., 7.]])
