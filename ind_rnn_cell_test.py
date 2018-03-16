38a5e0fb78d71520b29b1fb49090e7e5fe9ba652
batzner <git@kilians.net>
2018-03-16 17:42:05 +0100

    Add comments, change formatting
    This commit does not change the functionality.

--- ind_rnn_cell_test.py
+++ ind_rnn_cell_test.py
@@ -1,17 +1,20 @@
+"""Tests for the IndRNN cell."""
+
 import numpy as np
 
 from tensorflow.python.ops import variable_scope
 from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import rnn_cell_impl
-from tensorflow.python.ops import variables as variables_lib
+from tensorflow.python.ops import variables
 from tensorflow.python.platform import test
 
 from ind_rnn_cell import IndRNNCell
 
 
-class RNNCellTest(test.TestCase):
+class IndRNNCellTest(test.TestCase):
   def testIndRNNCell(self):
+    """Tests cell with recurrent weights exceeding the bounds."""
+
     with self.test_session() as sess:
       with variable_scope.variable_scope(
           "root", initializer=init_ops.constant_initializer(1.)):
@@ -21,9 +24,10 @@ class RNNCellTest(test.TestCase):
         cell = IndRNNCell(4, recurrent_min_abs=1., recurrent_max_abs=3.,
                           recurrent_initializer=recurrent_init)
         output, _ = cell(x, m)
-        sess.run([variables_lib.global_variables_initializer()])
+        sess.run([variables.global_variables_initializer()])
         res = sess.run([output], {x.name: np.array([[1., 1., 1., 1.]]),
-                                  m.name: np.array([[2., 2., 2., 2.]])})
-        # Recurrent Weights u should be -3, -2, 1, 3
-        # Pre-activations (4 + 2*u) should be -2, 0, 6, 10
+          m.name: np.array([[2., 2., 2., 2.]])})
+        # Recurrent Weights should be -3, -2, 1, 3
+        # (Pre)activations (4 + 2*rec_weight) should be -2, 0, 6, 10
+        # Thus, ReLU outputs should be 0, 0, 6, 10
         self.assertAllEqual(res[0], [[0., 0., 6., 10.]])
